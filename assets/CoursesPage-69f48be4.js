222af71c7826247ecd19abf47c8877996ff8efd2
verozroke <verozroke@users.noreply.github.com>
2023-03-03 18:08:28 +0000

    deploy: d2a57f90f30d8949c8ed068c48efeacd4733f3c2

--- assets/CoursesPage-69f48be4.js
+++
@@ -1,1 +0,0 @@
-import{C as e}from"./CoursesChoose-3afcd9de.js";import{_ as o,e as s,o as _,c as a,f as t}from"./index-3db1b9e6.js";const r={__name:"CoursesPage",setup(c){return s(()=>{window.scrollTo(0,0)}),(n,u)=>(_(),a("div",null,[t(e)]))}},d=o(r,[["__scopeId","data-v-e75fb891"]]);export{d as default};

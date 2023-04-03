666d3c8728391e339498f59c72a182925a3f878d
verozroke <verozroke@users.noreply.github.com>
2023-04-03 09:21:35 +0000

    deploy: 3847f2e1d7086c64a02f62a69cc96964650a9f61

---
+++ assets/CoursesPage-567fb209.js
@@ -0,0 +1,1 @@
+import{C as e}from"./CoursesChoose-c6ce32ff.js";import{_ as o,e as s,o as _,c as a,f as t}from"./index-ffe81bdb.js";const r={__name:"CoursesPage",setup(c){return s(()=>{window.scrollTo(0,0)}),(n,u)=>(_(),a("div",null,[t(e)]))}},d=o(r,[["__scopeId","data-v-e75fb891"]]);export{d as default};
--- assets/CoursesPage-986be243.js
+++
@@ -1,1 +0,0 @@
-import{C as e}from"./CoursesChoose-9c42bbea.js";import{_ as o,e as s,o as _,c as a,f as t}from"./index-9a379ab3.js";const r={__name:"CoursesPage",setup(c){return s(()=>{window.scrollTo(0,0)}),(n,u)=>(_(),a("div",null,[t(e)]))}},d=o(r,[["__scopeId","data-v-e75fb891"]]);export{d as default};

e137b91b05f2e0b896fd0c40e8dc40312c12d90f
verozroke <73778760+verozroke@users.noreply.github.com>
2023-05-25 13:01:19 +0600

    исправил инк

--- vite.config.js
+++ vite.config.js
@@ -9,18 +9,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@routes': path.resolve(__dirname, './src/routes'),
-      '@component': path.resolve(__dirname, './src/components'),
-      '@stores': path.resolve(__dirname, './src/stores'),
-      '@img': path.resolve(__dirname, './src/img'),
-      '@helpers': path.resolve(__dirname, './src/helpers'),
-      '@styles': path.resolve(__dirname, './src/styles'),
-    }
-  },
+  
 })
 
 
